b8d8e41e5479600b65989f5bca8ef325d0e2085d
chen <chen@123.com>
2019-12-17 09:54:19 +0800

    平台转移开发工作者

--- Main.js
+++ Main.js
@@ -9,7 +9,8 @@ export class Main{
     constructor(){
         console.log("Main执行了");
         //获取canvas
-        this.canvas = document.getElementById("game");
+        this.canvas = wx.createCanvas();
+        // this.canvas = document.getElementById("game");
         this.ctx = this.canvas.getContext('2d');
         //初始化资源加载器
         this.loader = new ResourcrLoader();
@@ -49,13 +50,14 @@ export class Main{
     }
     addClick(){
         
-        this.canvas.addEventListener('touchstart',e=>{
+        // this.canvas.addEventListener('touchstart',e=>{
+          wx.onTouchStart(e=>{
             //点击事件有两个效果
             //1.游戏结束。点击重新开始
             //2.游戏进行中，点击小鸟向上一段距离
             if(this.director.isGameOver){
                 //游戏结束
-
+             this.init();
             }else{
                 //游戏进行中 小鸟上飞一短距离
                 this.director.birdsEvent();
